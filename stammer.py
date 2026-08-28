79e54c52103cd05208052ad65a589865b23244e2
lizardgai4 <lizardgai4@gmail.com>
2026-08-28 11:40:47 -0700

    Reverted black's changes in stammer.py to avoid merge conflicts

--- stammer.py
+++ stammer.py
@@ -1,37 +1,33 @@
 #!/usr/bin/env python3
 
-import io
-import logging
-import os
-import subprocess
-import tempfile
 from argparse import ArgumentParser
-from pathlib import Path
 from typing import List
-
 import numpy as np
-from PIL import Image
 from scipy.io import wavfile
+from pathlib import Path
+import subprocess
+import io
+import os
 
-import fraction_bits
+from PIL import Image
+import tempfile
+import logging
+
 import image_tiling
+import fraction_bits
+from audio_matching import BasicAudioMatcher, CombinedFrameAudioMatcher, UniqueAudioMatcher, WeightedAudioMatcher
 import video_out
-from audio_matching import (
-    BasicAudioMatcher,
-    CombinedFrameAudioMatcher,
-    UniqueAudioMatcher,
-    WeightedAudioMatcher,
-)
 from video_out import VideoHandler, VideoHandlerDisk, VideoHandlerMem
 
-TEMP_DIR = Path("temp")
+
+TEMP_DIR = Path('temp')
 
 MAX_BASIS_WIDTH = 6
 MAX_TESSELLATION_COUNT = 9
-DEFAULT_FRAME_LENGTH = 1 / 25  # Seconds
+DEFAULT_FRAME_LENGTH = 1/25 # Seconds
 
 BAND_WIDTH = 1.2
-INTERNAL_SAMPLERATE = 44100  # Hz
+INTERNAL_SAMPLERATE = 44100 # Hz
 
 
 # max number of frames stored in memory
@@ -47,7 +43,6 @@ COMMON_AUDIO_EXTS = [
     "opus",
 ]
 
-
 def test_command(cmd):
     try:
         subprocess.run(cmd, capture_output=True)
@@ -55,43 +50,34 @@ def test_command(cmd):
         logging.error(f"ERROR: '{cmd[0]}' not found. Please install it.")
         raise error
 
-
 def file_type(path):
     # is the file at path an audio file, video file, or neither?
     return subprocess.run(
         [
-            "ffprobe",
-            "-loglevel",
-            "error",
-            "-show_entries",
-            "stream=codec_type",
-            "-of",
-            "csv=p=0",
-            str(path),
+            'ffprobe',
+            '-loglevel', 'error',
+            '-show_entries', 'stream=codec_type',
+            '-of', 'csv=p=0',
+            str(path)
         ],
         capture_output=True,
         check=True,
-        text=True,
+        text=True
     ).stdout
-
 
 def get_duration(path):
     return subprocess.run(
-        [
-            "ffprobe",
-            "-i",
-            str(path),
-            "-show_entries",
-            "format=duration",
-            "-v",
-            "quiet",
-            "-of",
-            "csv=p=0",
-        ],
-        capture_output=True,
-        check=True,
-        text=True,
-    ).stdout
+            [
+                'ffprobe',
+                '-i', str(path),
+                '-show_entries', 'format=duration',
+                '-v', 'quiet',
+                '-of', 'csv=p=0'
+            ],
+            capture_output=True,
+            check=True,
+            text=True
+        ).stdout
 
 
 def get_framecount(path):
@@ -187,24 +173,13 @@ def build_output_video(video_handler: VideoHandler, matcher, index_conversion):
         for video_frame_i in range(video_handler.best_match_count):
             elapsed_time = video_frame_i * video_frame_length
             audio_frame_i = int(elapsed_time / audio_frame_length)
-            time_past_start_of_audio_frame = elapsed_time - (
-                audio_frame_i * audio_frame_length
-            )
+            time_past_start_of_audio_frame = elapsed_time - (audio_frame_i * audio_frame_length)
             match_row = best_matches[audio_frame_i]
-            match_row = [
-                int(
-                    (i * audio_frame_length + time_past_start_of_audio_frame)
-                    / video_frame_length
-                )
-                for i in match_row
-            ]
-            tesselate_composite(
-                match_row, basis_coefficients[audio_frame_i], video_frame_i
-            )
+            match_row = [int((i * audio_frame_length + time_past_start_of_audio_frame)/video_frame_length) for i in match_row]
+            tesselate_composite(match_row, basis_coefficients[audio_frame_i], video_frame_i)
 
     # signals VideoHandlerDisk to start encoding
     video_handler.complete()
-
 
 def is_audio_filename(name):
     _path = Path(name)
@@ -214,57 +189,34 @@ def is_audio_filename(name):
 
     return ext in COMMON_AUDIO_EXTS
 
-
 def get_audio_as_wav_bytes(path):
-    ff_out = bytearray(
-        subprocess.check_output(
-            [
-                "ffmpeg",
-                "-hide_banner",
-                "-loglevel",
-                "error",
-                "-i",
-                str(path),
-                "-vn",
-                "-map",
-                "0:a:0",
-                "-ac",
-                "1",
-                "-ar",
-                str(INTERNAL_SAMPLERATE),
-                "-c:a",
-                "pcm_s16le",
-                "-f",
-                "wav",
-                "-",
-            ]
-        )
-    )
+    ff_out = bytearray(subprocess.check_output(
+        [
+            'ffmpeg',
+            '-hide_banner',
+            '-loglevel', 'error',
+            '-i', str(path),
+            '-vn', '-map', '0:a:0',
+            '-ac', '1',
+            '-ar', str(INTERNAL_SAMPLERATE),
+            '-c:a', 'pcm_s16le',
+            '-f', 'wav', '-'
+        ]
+    ))
 
     # fix file size in header length
-    actual_data_len = len(ff_out) - 44
-    ff_out[4:8] = (actual_data_len).to_bytes(4, byteorder="little")
+    actual_data_len = len(ff_out)-44
+    ff_out[4:8] = (actual_data_len).to_bytes(4,byteorder="little")
 
     return io.BytesIO(bytes(ff_out))
-
 
 def chunks_of_n(input_list, n):
     chunks = []
     for i in range(0, len(input_list), n):
-        chunks.append(input_list[i : n + i])
+        chunks.append(input_list[i:n+i])
     return chunks
 
-
-def process(
-    carrier_path,
-    modulator_path,
-    output_path,
-    custom_frame_length,
-    matcher_mode,
-    video_mode,
-    color_mode,
-    min_cached_frames,
-):
+def process(carrier_path, modulator_path, output_path, custom_frame_length, matcher_mode, video_mode, color_mode, min_cached_frames):
     if not carrier_path.is_file():
         raise FileNotFoundError(f"Carrier file {carrier_path} not found.")
     if not modulator_path.is_file():
@@ -274,12 +226,12 @@ def process(
     carrier_duration = float(get_duration(carrier_path))
     modulator_duration = float(get_duration(modulator_path))
 
-    video_in_mem = video_mode == "mem_decay"
+    video_in_mem = (video_mode == "mem_decay")
 
-    if "video" in carrier_type:
-
+    if 'video' in carrier_type:
+    
         logging.info("Calculating video length")
-
+    
         carrier_framecount = float(get_framecount(carrier_path))
         video_frame_length = carrier_duration / carrier_framecount
         if custom_frame_length is None:
@@ -287,16 +239,12 @@ def process(
         else:
             frame_length = float(custom_frame_length)
 
-    if "video" not in carrier_type and "audio" not in carrier_type:
-        logging.error(
-            f"Unrecognized file type: {carrier_path}. Should be audio or video"
-        )
+    if 'video' not in carrier_type and 'audio' not in carrier_type:
+        logging.error(f"Unrecognized file type: {carrier_path}. Should be audio or video")
         return
 
-    if not (("video" in modulator_type) or ("audio" in modulator_type)):
-        logging.error(
-            f"Unrecognized file type: {modulator_path}. Should be audio or video"
-        )
+    if not (('video' in modulator_type) or ('audio' in modulator_type)):
+        logging.error(f"Unrecognized file type: {modulator_path}. Should be audio or video")
         return
     frame_length = min(frame_length, carrier_duration / 3)
     frame_length = min(frame_length, modulator_duration / 3)
@@ -304,28 +252,22 @@ def process(
     _, carrier_audio = wavfile.read(get_audio_as_wav_bytes(carrier_path))
     _, modulator_audio = wavfile.read(get_audio_as_wav_bytes(modulator_path))
 
+
     logging.info("analyzing audio")
     if matcher_mode == "basic":
-        matcher = BasicAudioMatcher(
-            carrier_audio, modulator_audio, INTERNAL_SAMPLERATE, frame_length
-        )
+        matcher = BasicAudioMatcher(carrier_audio, modulator_audio, INTERNAL_SAMPLERATE, frame_length)
     elif matcher_mode == "combination":
-        matcher = CombinedFrameAudioMatcher(
-            carrier_audio, modulator_audio, INTERNAL_SAMPLERATE, frame_length
-        )
+        matcher = CombinedFrameAudioMatcher(carrier_audio, modulator_audio, INTERNAL_SAMPLERATE, frame_length)
     elif matcher_mode == "unique":
-        matcher = UniqueAudioMatcher(
-            carrier_audio, modulator_audio, INTERNAL_SAMPLERATE, frame_length
-        )
+        matcher = UniqueAudioMatcher(carrier_audio, modulator_audio, INTERNAL_SAMPLERATE, frame_length)
     elif matcher_mode == "weighted":
-        matcher = WeightedAudioMatcher(
-            carrier_audio, modulator_audio, INTERNAL_SAMPLERATE, frame_length
-        )
+        matcher = WeightedAudioMatcher(carrier_audio, modulator_audio, INTERNAL_SAMPLERATE, frame_length)
 
     logging.info("creating output audio")
-    matcher.make_output_audio(TEMP_DIR / "out.wav")
+    matcher.make_output_audio(TEMP_DIR / 'out.wav')
 
     index_conversion = {}
+
 
     if "video" in carrier_type:
         used_frames = sorted(set(matcher.best_matches))
@@ -386,7 +328,7 @@ def process(
                     )
             print()  # free newline? how convenient
 
-    elif "audio" in carrier_type:
+    elif 'audio' in carrier_type:
         carrier_is_video = False
         if custom_frame_length is None:
             frame_length = DEFAULT_FRAME_LENGTH
@@ -395,112 +337,49 @@ def process(
 
     if carrier_is_video:
         if video_mode == "mem_decay":
-            handler = VideoHandlerMem(
-                carrier_path,
-                output_path,
-                TEMP_DIR,
-                matcher,
-                carrier_framecount,
-                video_frame_length,
-                color_mode,
-            )
+            handler = VideoHandlerMem(carrier_path,output_path,TEMP_DIR,matcher,carrier_framecount,video_frame_length,color_mode)
             handler.cache.decay = MEM_DECAY_MAX
             handler.set_min_cached_frames(min_cached_frames)
         elif video_mode == "disk":
-            handler = VideoHandlerDisk(
-                carrier_path,
-                output_path,
-                TEMP_DIR,
-                matcher,
-                carrier_framecount,
-                video_frame_length,
-                color_mode,
-            )
+            handler = VideoHandlerDisk(carrier_path,output_path,TEMP_DIR,matcher,carrier_framecount,video_frame_length,color_mode)
 
         build_output_video(handler, matcher, index_conversion)
     else:
         subprocess.run(
             [
-                "ffmpeg",
-                "-loglevel",
-                "error",
-                "-y",
-                "-i",
-                str(TEMP_DIR / "out.wav"),
-                str(output_path),
+                'ffmpeg',
+                '-loglevel', 'error',
+                '-y', '-i', str(TEMP_DIR / 'out.wav'),
+                str(output_path)
             ],
-            check=True,
+            check=True
         )
 
-
 def main():
-    logging.basicConfig(format="%(message)s", level=logging.INFO)
+    logging.basicConfig(format='%(message)s', level=logging.INFO)
 
     # check required command line tools
-    test_command(["ffmpeg", "-version"])
-    test_command(["ffprobe", "-version"])
+    test_command(['ffmpeg', '-version'])
+    test_command(['ffprobe', '-version'])
 
     parser = ArgumentParser()
-    parser.add_argument(
-        "carrier_path",
-        type=Path,
-        metavar="carrier_track",
-        help="path to an audio or video file that frames will be taken from",
-    )
-    parser.add_argument(
-        "modulator_path",
-        type=Path,
-        metavar="modulator_track",
-        help="path to an audio or video file that will be reconstructed using the carrier track",
-    )
-    parser.add_argument(
-        "output_path",
-        type=Path,
-        metavar="output_file",
-        help="path to file that will be written to; should have an audio or video file extension (such as .wav, .mp3, .mp4, etc.)",
-    )
-    parser.add_argument(
-        "--custom-frame-length",
-        "-f",
-        help="uses this number as frame length, in seconds. defaults to 0.04 seconds (1/25th of a second) for audio, or the real frame rate for video",
-    )
-    parser.add_argument(
-        "-vm",
-        "--video_mode",
-        choices=("disk", "mem_decay"),
-        default="disk",
-        help="How STAMMER will store video frames internally.\
+    parser.add_argument('carrier_path', type=Path, metavar='carrier_track', help='path to an audio or video file that frames will be taken from')
+    parser.add_argument('modulator_path', type=Path, metavar='modulator_track', help='path to an audio or video file that will be reconstructed using the carrier track')
+    parser.add_argument('output_path', type=Path, metavar='output_file', help='path to file that will be written to; should have an audio or video file extension (such as .wav, .mp3, .mp4, etc.)')
+    parser.add_argument('--custom-frame-length', '-f', help='uses this number as frame length, in seconds. defaults to 0.04 seconds (1/25th of a second) for audio, or the real frame rate for video')
+    parser.add_argument('-vm', '--video_mode', choices=('disk', 'mem_decay'), default='disk', help='How STAMMER will store video frames internally.\
                         disk: Copy all frames to temp directory.\
-                        mem_decay: Decode frames into memory as needed and deletes unused frames over time. Recommended for very large videos.",
-    )
-    parser.add_argument(
-        "-mcf",
-        "--min_cached_frames",
-        type=int,
-        default=2,
-        help='Only applies to "mem_decay" video mode. Minimum number of frames STAMMER will cache for one decayed frame.',
-    )
-    parser.add_argument(
-        "-c",
-        "--color_mode",
-        choices=("8fast", "8full", "full"),
-        default="full",
-        help="Bitdepth of internal video frames.\
+                        mem_decay: Decode frames into memory as needed and deletes unused frames over time. Recommended for very large videos.')
+    parser.add_argument('-mcf', '--min_cached_frames', type=int, default=2, help='Only applies to "mem_decay" video mode. Minimum number of frames STAMMER will cache for one decayed frame.')
+    parser.add_argument('-c', '--color_mode', choices=('8fast', '8full', 'full'), default='full', help='Bitdepth of internal video frames.\
                         8fast: generates 8-bit PNGs with default palette, fast and low filesize but low-quality. \
                         8full: generates 8-bit PNGs with a custom 256-color palette for each frame. slow but looks great. \
-                        full: generates 16-bit PNGs, default. fast and looks good, but high filesize.",
-    )
-    parser.add_argument(
-        "-m",
-        "--matcher_mode",
-        choices=("basic", "combination", "unique", "weighted"),
-        default="basic",
-        help="""Which algorithm Stammer will use.
+                        full: generates 16-bit PNGs, default. fast and looks good, but high filesize.')
+    parser.add_argument('-m', '--matcher_mode', choices=('basic', 'combination', 'unique', 'weighted'), default='basic', help="""Which algorithm Stammer will use.
         basic: replace each frame in the modulator with the most similar frame in the carrier.
         combination: replace each frame in the modulator with a linear combination of several frames in the carrier, to more closely approximate it.
         unique: limit each carrier frame to only appear once. If the carrier is longer than the modulator, some carrier frames will not be played, if it is shorter than the modulator, the modulator will be trimmed to the length of the carrier.
-        weighted: apply an A-weighting curve to the audio spectra, to try and make formants more similar.""",
-    )
+        weighted: apply an A-weighting curve to the audio spectra, to try and make formants more similar.""")
     args = parser.parse_args()
     with tempfile.TemporaryDirectory() as tempdir:
         global TEMP_DIR
@@ -508,5 +387,5 @@ def main():
         process(**vars(args))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
